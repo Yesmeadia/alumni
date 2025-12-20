7848ea6da89ebbb7f627021a3ff8293921139636
Yesmeadia <yesmediaofficial@gmail.com>
2025-12-21 03:02:44 +0530

    fixed commit

--- html2canvas.d.ts
+++
@@ -1,9 +0,0 @@
-// html2canvas.d.ts
-import 'html2canvas';
-
-// Extend the existing options interface
-declare module 'html2canvas' {
-  interface Html2CanvasOptions {
-    scale?: number; 
-  }
-}
